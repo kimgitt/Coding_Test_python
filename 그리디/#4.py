c8279f1a14de2fcdb2da42d0f3f3999031960117
kimgitt <124108710+kimgitt@users.noreply.github.com>
2023-03-14 00:10:47 +0900

    Create #4.py

---
+++ 그리디/#4.py
@@ -0,0 +1,16 @@
+# 1이 될 때까지
+n ,k = map(int, input().split())
+cnt = 0
+while True:
+    if n % k == 0:
+        n = n // k
+        cnt += 1
+        if n == 1:
+            break
+    else:
+        n = n -1 
+        cnt += 1
+        if n == 1:
+            break
+
+print(cnt)
